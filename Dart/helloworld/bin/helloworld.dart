1b012b793df130507507621a186c425de0b4ceb8
Seidecaru Daniel <seidecarudaniel@mail.com>
2021-11-05 16:12:57 +0200

    Number variable lesson
    The Lesson on  number variables

--- Dart/helloworld/bin/helloworld.dart
+++ Dart/helloworld/bin/helloworld.dart
@@ -8,11 +8,30 @@ void main(List<String> arguments) {
    */
 
   // bools section lesson
-  bool? isOn; // a boolian variable
+  /*bool? isOn; // a boolian variable
   bool isDog = false;
 
   print(isOn);
   print('isDog = ${isDog}');
-  print('isOn is a ${isOn.runtimeType}');
+  print('isOn is a ${isOn.runtimeType}'); */
 
+  // number variables section
+  int age = 32;
+
+  // int
+  int people = 6;
+  // double
+  double temp = 32.06;
+
+  // Parse as int
+  int test = int.parse('12');
+  print(test);
+
+
+  int error = int.parse('12.08', onError: (source) => 0);
+  print('error = $error');
+
+  int dogYears = 7;
+  int dogAge = age * dogYears;
+  print('Your age in dog years is: $dogAge');
 }
